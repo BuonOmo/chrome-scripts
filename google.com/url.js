60c725ac4b7c909803eca03ba16ef308541198d2
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2022-10-21 12:21:14 +0200

    feat(google.com): Automatically open redirects

---
+++ google.com/url.js
@@ -0,0 +1,3 @@
+// @include _scripts/wait-for.js
+
+waitFor('a').then(function(a) { a.click() })
---
+++ www.google.com/url.js
@@ -0,0 +1,3 @@
+// @include _scripts/wait-for.js
+
+waitFor('a').then(function(a) { console.log("hey"); a.click() })
